ccb6709b21bb5e82b2154b7a501ccffdcb62d51c
Lucas-CCV <lucasccv.2@hotmail.com>
2025-05-27 16:45:11 -0300

    adicionado o modelo do chat

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from typing import List
 
-import pygame
 import sys
 from enum import Enum
 import socket
 import threading
 
 from nltk.downloader import update
+
 from pygame.rect import RectType
 from pygame.time import Clock
+from pygame.font import Font
 from pygame import Surface
+import pygame
 
 WINDOW_WIDTH  = 1000
 WINDOW_HEIGHT = 600
@@ -37,38 +39,122 @@ BTN_COLORS = (0x7c, 0x7F, 0xEA)
 # Janela
 pygame.display.set_caption("Seega")
 
+
+def fit_text_size(text, font, max_width, invert):
+    tamanho: int = 0
+
+    # enquanto o tamanho que cabe do texto não tiver chegado no ultimo caractere e o tamanho do texto não tiver passado o max_width
+    while tamanho < len(text) and font.size((text[-(tamanho + 1):]) if invert else (text[:tamanho + 1]))[
+        0] <= max_width:
+        # incrementa uma letra que ira ser mostrada
+        tamanho += 1
+
+    return tamanho
+
+
+def split_text_to_fit(text, font, max_width):
+    lines = []
+    index = 0
+    tamanho = 0
+
+    while True:
+        # pega a quantidade de caracteres que cabem no max_width
+        tamanho = fit_text_size(text[index:], font, max_width, False)
+        # salva essa parte do texto em uma lista
+        lines.append(text[index:index + tamanho])
+        # salva a posição inicial do novo texto
+        index = index + tamanho
+
+        # se tiver chegado ou passado do final do texto
+        if index >= len(text):
+            break
+
+    return lines
+
 class Buttons:
     pass
 
 class Chat:
-    pass
+    def __init__(self, window: Surface, top_margin: int, left_margin: int, chat_height  :int, chat_width   :int):
+        self.window :Surface = window
+
+        self.top_margin  :int = top_margin
+        self.left_margin :int = left_margin
+        self.chat_height :int = chat_height
+        self.chat_width  :int = chat_width
+
+        self.font :Font = pygame.font.SysFont("Arial", 18)
+
+        self.max_messages :int = 17
+
+        self.chat_messages :List[str] = []
+        self.input_text :str = ""
+
+
+    def draw_chat(self):
+        # desenha um retângulo preto que seria a borda do char e um em cima dele do plano de fundo do chat
+        pygame.draw.rect(self.window, WHITE, (self.left_margin, self.top_margin,
+                                                   self.chat_width, self.chat_height))
+        pygame.draw.rect(self.window, BLACK, (self.left_margin, self.top_margin,
+                                                   self.chat_width, self.chat_height), 2)
+
+        # pega o topo do chat e cria uma margem
+        height = self.top_margin + 5
+        for msg in self.chat_messages[-self.max_messages:]:
+            # renderiza o texto, o coloca na tela e pega o tamanho da altura do texto,
+            # cria uma margem para a proxima mensagem
+            text_surface = self.font.render(msg, True, BLACK)
+            self.window.blit(text_surface, (self.left_margin + 5,height))
+            height += self.font.get_height() + 5  # adiciona altura da fonte + espaço entre linhas
+
+        # faz o retângulo do input
+        input_rect = pygame.Rect(self.left_margin, self.top_margin + self.chat_height + 5, self.chat_width, 30)
+        pygame.draw.rect(self.window, WHITE, input_rect)
+        pygame.draw.rect(self.window, BLACK, input_rect, 2)
+
+        # renderiza o texto de input somente da parte final que caiba no texto e o coloca na tela
+        input_surface = self.font.render(
+            self.input_text[-fit_text_size(self.input_text, self.font, self.chat_width - 10, True):],
+            True, BLACK)
+        self.window.blit(input_surface, (input_rect.x + 5, input_rect.y + 5))
+
+
+    def add_chat_messages(self, origin:str, texto:str):
+        # pega o texto para ser adicionado no chat e verifica e separa ele caso não caiba em somente uma linha
+        message = split_text_to_fit(f"{origin}: {texto.strip()}", self.font, self.chat_width - 10)
+
+        for text in message:
+            # salva nas mensagens do chat o texto (ou a parte do texto que caiba)
+            self.chat_messages.append(text)
+            if len(self.chat_messages) > self.max_messages:
+                # se a quantidade de texto for maior que o maximo de mensagens no chat, tira a primeira da fila
+                self.chat_messages.pop(0)
+
+
 
 class Player:
     def __init__(self):
-        self.pieces :List[Pieces] = []
-
-        self.points         :int = 0
-
-        self.color :tuple[int,int,int] = (0x0, 0x0, 0x0)
+        self.points :int = 0
+        self.color  :tuple[int,int,int] = (0x0, 0x0, 0x0)
 
 
 
 class Board:
-    def __init__(self, window: Surface):
-        self.window = window
+    def __init__(self, window :Surface, board_size :int, left_margin :int, top_margin :int,square_quant_pixels :int):
+        self.window :Surface = window
 
-        self.board_size          :int = 5
-        self.board_left_margin   :int = 20
-        self.square_quant_pixels :int = 100
+        self.board_size          :int = board_size
+        self.top_margin          :int = top_margin
+        self.left_margin         :int = left_margin
+        self.square_quant_pixels :int = square_quant_pixels
 
         self.selected_piece      :List[int] = [-1, -1]
 
         self.board: List[List[int]] = [[-1 for _ in range(self.board_size)] for _ in range(self.board_size)]
 
-        board_pixels_size      :int = self.board_size*self.square_quant_pixels
-        self.board_top_margin  :int = (self.window.get_height() - board_pixels_size) // 2 - 30
 
-        self.board_background_rect :RectType = pygame.Rect(self.board_left_margin, self.board_top_margin,
+
+        self.board_background_rect :RectType = pygame.Rect(self.left_margin, self.top_margin,
                                                            self.board_size*self.square_quant_pixels,
                                                            self.board_size*self.square_quant_pixels)
 
@@ -79,8 +165,8 @@ class Board:
         for row in range(self.board_size):
             for col in range(self.board_size):
                 rect = pygame.Rect(
-                    self.board_left_margin + col * self.square_quant_pixels,
-                    self.board_top_margin + row * self.square_quant_pixels,
+                    self.left_margin + col * self.square_quant_pixels,
+                    self.top_margin + row * self.square_quant_pixels,
                     self.square_quant_pixels,
                     self.square_quant_pixels
                 )
@@ -92,18 +178,31 @@ class Board:
                         color = color[1] + 50
                     pygame.draw.circle(self.window, color, rect.center, self.square_quant_pixels // 3)
 
-class Pieces:
-    pass
+
 
 class Window:
-    def __init__(self):
+    def __init__(self, width: int, height: int):
         self.general_font = pygame.font.SysFont("Arial", 20)
-        self.input_font   = pygame.font.SysFont("Arial", 18)
 
-        self.window : Surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.window : Surface = pygame.display.set_mode((width, height))
 
-        self.board  : Board   = Board(self.window)
-        self.chat   : Chat    = Chat()
+        board_size          :int = 5
+        top_margin          :int = 20
+        side_margin         :int = 20
+        square_quant_pixels :int = 100
+
+        chat_left_margin :int = side_margin + board_size*square_quant_pixels+side_margin
+        chat_height      :int = board_size*square_quant_pixels
+        chat_width       :int = self.window.get_width() - chat_left_margin - side_margin
+
+        self.board  : Board   = Board(self.window, board_size, side_margin, top_margin, square_quant_pixels)
+        self.chat   : Chat    = Chat(self.window, top_margin, chat_left_margin, chat_height, chat_width)
+
+
+    def update_window(self, board_color:tuple[int, int, int], players: List[Player]):
+        self.window.fill(BG_COLOR)
+        self.board.draw_board(board_color, players)
+        self.chat.draw_chat()
 
 
 
@@ -119,26 +218,22 @@ class Game:
         self.players :List[Player] = []
 
         self.clock:Clock = pygame.time.Clock()
-        self.window: Window = Window()
+        self.window: Window = Window(1000, 600)
 
-
-    def run_game(self):
-        while self.run:
-            self.update_display()
-
-    def update_display(self):
+    def show_screen(self):
         if self.game_state == -1:
             board_color = BLACK
         else:
             board_color = self.players[self.current_player].color
 
         self.clock.tick(60)
-
-        self.window.window.fill(BG_COLOR)
-
-        self.window.board.draw_board(board_color, self.players)
-
+        self.window.update_window(board_color, self.players)
         pygame.display.flip()
+
+    def run_game(self):
+        while self.run:
+            self.show_screen()
+
 
 
 class PlayersTurnStatus(Enum):
