888e4e4f8cc3d793ae097e119bb78cabc047b156
Lucas-CCV <lucasccv.2@hotmail.com>
2025-06-15 22:43:03 -0300

    comentarios

--- main.py
+++ main.py
@@ -32,6 +32,16 @@ game  : Game
 
 
 def fit_text_size(text, font, max_width, invert):
+    """
+    funcao extra para saber quantas letras de um texto com uma determinada fonte cabe em uma area especifica
+
+    :param text: texto para caber
+    :param font: font que vai ser usada
+    :param max_width: tamanho que o texto deve caber
+    :param invert: se a verificacao deve comecar da direita ou da esquerda
+    :return: quantas letras cabem nesse espaco
+    """
+
     tamanho: int = 0
 
     # enquanto o tamanho que cabe do texto não tiver chegado no último caractere e o tamanho do texto não
@@ -46,6 +56,15 @@ def fit_text_size(text, font, max_width, invert):
 
 
 def split_text_to_fit(text, font, max_width):
+    """
+    quebra o texto de uma determinada fonte em varios pacotes para caber em um tamanho especificado
+
+    :param text: texto para ser quebrado
+    :param font: font do texto
+    :param max_width: tamanho que deve caber
+    :return: os pacotes do texto quebrado
+    """
+
     lines = []
     index = 0
 
@@ -66,6 +85,16 @@ def split_text_to_fit(text, font, max_width):
 
 
 def draw_any_rect_with_text(window: Surface, font: Font,color:tuple[int, int, int], rect: RectType, text: str):
+    """
+    cria um retangulo na tela e escreve um texto dentro dele
+
+    :param window: qual a superfice o retangulo sera criado
+    :param font:  a fonte do texto que ira ser usada
+    :param color: a cor do retangulo
+    :param rect: definicao do retangulo
+    :param text: texto para ser escrito
+    """
+
     pygame.draw.rect(window, color, rect)
     pygame.draw.rect(window, BLACK, rect, 2)
 
@@ -76,8 +105,19 @@ def draw_any_rect_with_text(window: Surface, font: Font,color:tuple[int, int, in
 
 
 class Button:
+    """
+    classe para de cado um dos botoes individuais
+    """
     def __init__(self, window: Surface, font: Font ,text: str,
                  function: Callable, rect: RectType, color :tuple[int, int, int]):
+        """
+        :param window: surpeficie que os botoes serao utilizados
+        :param font: fonte dos textos dos botoes
+        :param text:  texto do botoes
+        :param function: funcao que ira acontecer ao clicar no botao
+        :param rect: objeto retangulo
+        :param color: cor do botao
+        """
         self.window   :Surface  = window
         self.font     :Font     = font
         self.rect     :RectType = rect
@@ -87,17 +127,36 @@ class Button:
 
 
     def draw(self):
+        """
+        desenha o botao
+        """
         draw_any_rect_with_text(self.window, self.font, self.color, self.rect, self.text)
 
 
     def click(self) -> Window.BTNPressed:
+        """
+        chama a funcao do botao
+        :return:
+        """
         return self.function()
 
 
 
 class Buttons:
+    """
+    classe para controlar todos os botoes
+    """
     def __init__(self, window: Surface, quant_buttons: int, text_list: List[str], function_list: List[Callable],
                  left_margin: int, top_margin: int, font: Font):
+        """
+        :param window: surpeficie que os botoes serao utilizados
+        :param quant_buttons: quantidade de botoes
+        :param text_list: lista com os textos de todos os botoes
+        :param function_list: lista com todos as funcoes de cada botao
+        :param left_margin: margem do botao ate o lado esquerdo
+        :param top_margin: margem ate o topo
+        :param font: fonte dos textos do botoes
+        """
         self.window : Surface = window
 
         self.buttons :List[Button] = []
@@ -112,6 +171,11 @@ class Buttons:
 
 
     def verify_btn(self, position=pygame.mouse.get_pos()) -> Window.BTNPressed:
+        """
+        verifica
+        :param position: posicao na qual o mouse clicou
+        :return: identificador de qual botao foi precionado
+        """
         for index in range(len(self.buttons)):
             if self.buttons[index].rect.collidepoint(position):
                 return self.buttons[index].click()
@@ -120,13 +184,26 @@ class Buttons:
 
 
     def draw_btns(self):
+        """
+        chama a funcao para desenhar cada um dos botoes
+        """
         for button in self.buttons:
             button.draw()
 
 
 
 class Chat:
+    """
+    faz o controle do chat de mostrar coisas na tela
+    """
     def __init__(self, window: Surface, top_margin: int, left_margin: int, chat_height  :int, chat_width   :int):
+        """
+        :param window: surpeficie que o chat vai ser desenhado
+        :param top_margin: margem ate o topo
+        :param left_margin: margem do botao ate o lado esquerdo
+        :param chat_height: altura do chat
+        :param chat_width: largura do chat
+        """
         self.window :Surface = window
 
         self.top_margin  :int = top_margin
@@ -145,6 +222,9 @@ class Chat:
 
 
     def draw_chat(self):
+        """
+        desenha o chat na superficie
+        """
         chat_messages_height = self.chat_height - self.input_chat_height - self.top_margin
 
         # desenha um retângulo preto que seria a borda do char e um em cima dele do plano de fundo do chat
@@ -189,7 +269,7 @@ class Chat:
     def get_chat_input(self, event) -> str:
         value: str
 
-        # verifica se algo do teclado foi precionado
+        # verifica se algo do teclado foi pressionado
         if event.type == pygame.KEYDOWN:
             # se for um backspace apaga da ultima posição
             if event.key == pygame.K_BACKSPACE:
@@ -202,7 +282,7 @@ class Chat:
                     self.input_text = ""
                     return value
             else:
-                # empede do usuário escrever algo com mais de 200 chars
+                # impede do usuário escrever algo com mais de 200 chars
                 if len(self.input_text) < 200:
                     self.input_text += event.unicode
 
@@ -211,14 +291,30 @@ class Chat:
 
 
 class Player:
+    """
+    classe pra controlar o player
+    """
     def __init__(self, color: tuple[int,int,int]):
+        """
+        :param color: cor das relacionada ao player
+        """
         self.points :int = 0
         self.color  :tuple[int,int,int] = color
 
 
 
 class Board:
+    """
+    classe para controlar o tabuleiro
+    """
     def __init__(self, window :Surface, board_size :int, left_margin :int, top_margin :int,square_quant_pixels :int):
+        """
+        :param window: surpeficie que o chat vai ser desenhado
+        :param board_size: quantidade de casa do tabuleiro de casa do tabuleiro
+        :param left_margin: margem a esquerda
+        :param top_margin:margem do topo
+        :param square_quant_pixels: tamanho de cada casa do tabuleiro
+        """
         self.window :Surface = window
 
         self.board_size          :int = board_size
@@ -252,6 +348,12 @@ class Board:
 
 
     def draw_board(self, board_color: tuple[int, int, int], players: List[Player]):
+        """
+        desenha o tabuleiro
+        :param board_color: cor dos quadrados do tabuleiro
+        :param players: informacoes do player
+        :return:
+        """
         pygame.draw.rect(self.window, BLACK, self.board_background_rect)
 
         # Tabuleiro
@@ -291,6 +393,11 @@ class Board:
 
 
     def get_board_position(self, pos):
+        """
+        pega em qual casa que foi clicada
+        :param pos: posicao que o mouse foi clicado
+        :return: a posicao dentro do tabuleiro
+        """
         x, y = pos
         col_init = (x - self.left_margin) // self.square_quant_pixels
         row_init = (y - self.top_margin) // self.square_quant_pixels
@@ -299,6 +406,13 @@ class Board:
 
 
     def remove_piece(self, row, col, current_player) -> bool:
+        """
+        verifica se pode e remove uma peca
+        :param row: qual casa da linha foi selecionada
+        :param col: qual casa da coluna foi selecionada
+        :param current_player: player jogando atualmente
+        :return: booleano dependendo se a peca foi removida ou nao
+        """
         removed = False
         test = [-1, 1]
 
@@ -324,7 +438,16 @@ class Board:
 
 
 class Window:
+    """
+    classe para controlar a tela em si
+    """
     def __init__(self, width: int, height: int, btn_text_list :List[str],btn_function_list: List[Callable]):
+        """
+        :param width: largura da tela
+        :param height: altura da tela
+        :param btn_text_list: lista dos textos dos botoes
+        :param btn_function_list: lista de funcoes dos botoes
+        """
         pygame.display.set_caption("Seega")
 
         self.general_font = pygame.font.SysFont("Arial", 20)
@@ -360,6 +483,11 @@ class Window:
 
 
     def update_window(self, board_color: tuple[int, int, int], players: List[Player]):
+        """
+        atualiza a tela
+        :param board_color: cores dos quadrados do tabuleiro
+        :param players: dados jogadores
+        """
         self.window.fill(BG_COLOR)
         self.board.draw_board(board_color, players)
         self.chat.draw_chat()
@@ -369,6 +497,9 @@ class Window:
 
 @expose
 class Interface:
+    """
+    classe de intermedio entre o cliente e o servidor
+    """
     @staticmethod
     def start(sistem_player: int = 0):
         global game
@@ -413,6 +544,9 @@ class Interface:
 
 
 class Game:
+    """
+    classe que controla o jogo
+    """
     class PlayersTurnStatus(Enum):
         ERROR                = 0
         MOVED_WITH_REMOVE    = 1
@@ -455,6 +589,11 @@ class Game:
 
 
     def get_id(self, input_string: str) -> str:
+        """
+        pega o identificador do usuário
+        :param input_string: texto para ser mostrado para o usuario
+        :return: retorna o identificador
+        """
         text: str = ""
 
         while self.run and text == "":
@@ -465,14 +604,20 @@ class Game:
                     if event.type == pygame.QUIT:
                         self.run = False
 
+                    #pega o texto do jogador
                     text = self.window.chat.get_chat_input(event)
 
             self.add_chat_messages(f"identificador selecionada {text}", "sistema")
 
+        # retorna o texto do jogador
         return text
 
 
     def verify_connection(self) -> bool:
+        """
+        verifica se ha uma coneccao
+        :return: booleano se ha ou nao coneccao
+        """
         if self.enemy_game is None:
             self.add_chat_messages("nenhum jogador conectado", "sistema")
             return False
@@ -480,6 +625,9 @@ class Game:
 
 
     def connect(self):
+        """
+        se conecta ao outro jogador
+        """
         self.add_chat_messages("tentando se conectar ao servidor", "sistema")
 
         while True:
@@ -494,6 +642,9 @@ class Game:
 
 
     def register(self):
+        """
+        registra no servidor de nome
+        """
         interface: Interface = Interface()
 
         daemon = Daemon()
@@ -516,6 +667,11 @@ class Game:
 
 
     def start(self,  sistem_player: int = 0) -> Window.BTNPressed:
+        """
+        funcao para iniciar o jogo
+        :param sistem_player: qual o identificador do player
+        :return: identificador do do botao precionado e nenhum botao caso nao haja coneccao
+        """
         print(f"start({sistem_player})")
         if not self.verify_connection():
             return Window.BTNPressed.NO_BTN
@@ -534,6 +690,11 @@ class Game:
 
 
     def give_up(self, sistem_player:int = PLAYER_ID) -> Window.BTNPressed:
+        """
+        jogador desistiu da partida
+        :param sistem_player: jogador que desistiu da partida
+        :return: identificador do do botao precionado e nenhum botao caso nao haja coneccao
+        """
         print(f"give_up({sistem_player})")
         if not self.verify_connection():
             return Window.BTNPressed.NO_BTN
@@ -548,6 +709,11 @@ class Game:
 
 
     def add_chat_messages(self, texto:str, origin:str="player"):
+        """
+        adiciona uma mensagem no chat
+        :param texto: texto para ser mostrado para o usuario
+        :param origin: origem do texto
+        """
         print("add_chat_messages")
         if origin == "player":
             if not self.verify_connection():
@@ -560,6 +726,12 @@ class Game:
 
 
     def reset(self, init:bool = False, send: bool = True) -> Window.BTNPressed:
+        """
+        reseta a partida
+        :param init: se é o comeco do programa ou nao
+        :param send: se e enviado o reset para o inimigo
+        :return:
+        """
         print(f"reset({init}, {send})")
         if not init and not self.verify_connection():
             return Window.BTNPressed.NO_BTN
@@ -588,6 +760,12 @@ class Game:
 
 
     def put_peace(self, row: int, col: int, send: bool = True):
+        """
+        coloca uma peca no tabuleiro
+        :param row: linha para colocar a peca
+        :param col: coluna para colocar a peca
+        :param send: se e para enviar ao inimigo
+        """
         print(f"put_peace({row}, {col}, {send})")
         if not self.verify_connection():
             return
@@ -600,6 +778,10 @@ class Game:
 
 
     def check_victory(self) -> bool:
+        """
+        virifica a condicao de um dos jogadores ja terem ganhado
+        :return: booleano dependendo se houve vitoria
+        """
         if self.game_state == 1 and (self.window.board.pieces_placed[0] == 0 or self.window.board.pieces_placed[1] == 0):
             self.add_chat_messages(f"vitoria do {"player" if self.sistem_player == self.current_player else "opponent"}", "sistema")
             self.players[PLAYER_ID if self.current_player == self.sistem_player else OPPONENT_ID].points += 1
@@ -608,6 +790,10 @@ class Game:
 
 
     def pass_turn(self, send: bool = True):
+        """
+        passa o turno para o outro jogador
+        :param send: se e para enviar ao inimigo
+        """
         print(f"pass_turn({send})")
         if not self.verify_connection():
             return
@@ -624,6 +810,15 @@ class Game:
 
 
     def move_peace(self, row_init: int, col_init: int, row_end: int, col_end: int, send: bool = True) -> bool:
+        """
+        move uma peca ja no tabuleiro
+        :param row_init: linha inicial
+        :param col_init: coluna inicial
+        :param row_end: linha final
+        :param col_end: coluna final
+        :param send: se e para enviar ao inimigo
+        :return:
+        """
         self.window.board.board[row_init][col_init] = -1
         self.window.board.board[row_end][col_end] = self.current_player
 
@@ -634,6 +829,10 @@ class Game:
 
 
     def cancel(self) -> Window.BTNPressed:
+        """
+        cancela a acao do usuario
+        :return: botao de identificador
+        """
         print("cancel->canceled")
         self.window.board.selected_piece = [-1, -1]
 
@@ -641,6 +840,11 @@ class Game:
 
 
     def handle_placement(self, pos) -> bool:
+        """
+        verifica se e uma posicao valida
+        :param pos: posicao
+        :return: booleano de se e ou nao a posicao valida
+        """
         col, row = self.window.board.get_board_position(pos)
 
         if 0 <= row < self.window.board.board_size and 0 <= col < self.window.board.board_size:
@@ -657,6 +861,9 @@ class Game:
 
 
     def show_screen(self):
+        """
+        atualiza a tela do jogo
+        """
         if self.game_state == -1:
             board_color = BLACK
         else:
@@ -668,6 +875,11 @@ class Game:
 
 
     def players_turn(self, pos) -> Game.PlayersTurnStatus:
+        """
+        controla o turno do jogador
+        :param pos: posicao que o player interagiu
+        :return: o que aconteceu dentro da jogada
+        """
         first_play = True
 
         col_init, row_init = self.window.board.get_board_position(pos)
@@ -731,6 +943,9 @@ class Game:
 
 
     def run_game(self):
+        """
+        gerencia o jogo no geral
+        """
         self.reset(True, False)
 
         while self.run:
